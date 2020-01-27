e3da42ce5b044f6efab3ff27dbef2c62ec892b7d
KANTE <ibrahimprincealexkante@gmail.com>
2020-01-27 21:18:21 +0100

    "Deuxième modification de AnalyticCounter"

--- Project02Eclipse/src/com/hemebiotech/classes/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/classes/AnalyticsCounter.java
@@ -1,43 +1,38 @@
 package com.hemebiotech.classes;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.FileWriter;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0; 		// initialize to 0
-	
-	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		String line = reader.readLine();
+import com.hemebiotech.interfaces.IAnalyticCounter;
 
-		int i = 0;	// set i to 0
-		int headCount = 0;	// counts headaches
-		while (line != null) {
-			i++;	// increment i
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headCount++;
-				System.out.println("number of headaches: " + headCount);
+/**
+ * SymptomCounter is the class representing a symptom counter.
+ * 
+ * @author masibak
+ * @version 1.0
+ */
+public class AnalyticsCounter implements IAnalyticCounter {
+	/**
+	 * count the number of occurrences of a list of Strings,
+	 * 
+	 * @param list
+	 * 			  the list of Strings
+	 * 
+	 * @return the symptom and its number of occurrences
+	 */
+	public Map<String, Integer> getStringOccurrencesFromList(List<String> list) {
+		Map<String,Integer> mapSymptomsOccurence = new HashMap<>();
+		for (String entry : list) { // Parcours de la liste entrée en paramètre de la fonction
+			// Cas 1 : l'entrée est déjà présente dans la liste
+			if(mapSymptomsOccurence.containsKey(entry)) {
+				mapSymptomsOccurence.put(entry,(mapSymptomsOccurence.get(entry)+1));
+			// Cas 2 : l'entrée n'est pas présente => on l'ajoute dans la map
+			} else {
+				mapSymptomsOccurence.put(entry,1);
 			}
-			else if (line.equals("rush")) {
-				rashCount++;
-			}
-			else if (line.contains("pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
-		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
-		writer.write("headache: " + headacheCount + "\n");
-		writer.write("rash: " + rashCount + "\n");
-		writer.write("dialated pupils: " + pupilCount + "\n");
-		writer.close();
+		}	
+		return mapSymptomsOccurence;
 	}
+	
 }
